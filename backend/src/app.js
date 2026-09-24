7e943a5d9447ad597cb110289fb0dbf7afc7e31d
Your Name <your@email.com>
2026-09-24 19:26:00 +0100

    feat: restore gitignore and an extra route in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ const authRoutes = require('./modules/auth/auth.routes');
 const projectRoutes = require('./modules/project/project.route');
 const notFound = require('./middleware/notFound.middleware');
 const { errorHandler } = require('./middleware/error.middleware');
+const googleRoutes = require('./modules/google-auth/google.routes');
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.get('/api/v1/health', (req, res) => {
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/projects', projectRoutes);
+app.use('/api/v1/google-auth', googleRoutes); // Add this line to include Google routes
 
 app.use(notFound);
 app.use(errorHandler);
